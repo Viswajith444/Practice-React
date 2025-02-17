0342a5f407901458f590d70b114ad1806531650a
Viswajith444 <viswajith108@gmail.com>
2025-02-17 14:13:43 +0530

    class demo

--- day7-javascrpt/callbackfn.js
+++ day7-javascrpt/callbackfn.js
@@ -8,3 +8,19 @@ function callbackfn(){
 }
 
 fn("welcome", callbackfn);
+
+class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+
+    greet() {
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+  }
+
+  const person1 = new Person("Alice", 25);
+
+  console.log(person1.name);
+  person1.greet();
