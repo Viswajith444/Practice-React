16535f71e0a60568c5ea4d20c7c9b7919ffcc04b
Viswajith444 <viswajith108@gmail.com>
2025-02-22 02:24:34 +0530

    dropbox HTML and CSS

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,21 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/navbar.jsx";
 import About from "./components/About.jsx";
+import Effect from "./components/Effect.jsx";
 
 function App() {
   // const [num, numfn] = useState(0);
+  let nav_items = [
+    { ref: "#", text: "Home" },
+    { ref: "#", text: "About" },
+    { ref: "#", text: "About" },
+    { ref: "#", text: "About" },
+  ];
+
   return (
     <main>
-      <Navbar />
+      <Navbar items={nav_items} last_nth={1}/>
       <About />
+      <Effect />
     </main>
   );
 }
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -1,24 +1,66 @@
-import "./style.css";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./styles/navbar.css";
 
-export default function Navbar() {
+function DropdownMenu() {
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
+    <ul className="dropdown-menu">
+      <li>
+        <a href="#">Hello</a>
+      </li>
+      <li>
+        <a href="#">World</a>
+      </li>
+    </ul>
+  );
+}
 
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Services</a>
-        </li>
+function Navbar({ items, last_nth, thickness }) {
+  let n = items.length;
+  // const [hoveredIndex, setHoveredIndex] = useState(null); // Track which item is hovered
 
-        <li>
-          <a href="#">Donate</a>
-        </li>
-      </ul>
+  let nav_items = items.map((ele, i) => (
+    <li
+      key={i}
+      // onMouseEnter={() => setHoveredIndex(i)}
+      // onMouseLeave={() => setHoveredIndex(null)}
+    >
+      <a href={ele.ref}> {ele.text} </a>
+
+      {/* {hoveredIndex === i && <DropdownMenu />} Only render dropdown on hover */}
+      <DropdownMenu />
+    </li>
+  ));
+
+  if (last_nth !== 0 && last_nth !== undefined && last_nth < n) {
+    let obj = items[n - last_nth];
+    nav_items[n - last_nth] = (
+      <li key={n - last_nth} style={{ marginLeft: "auto" }}>
+        <a href={obj.ref}>{obj.text}</a>
+      </li>
+    );
+  }
+
+  return (
+    <nav
+      className="navbar"
+      style={thickness ? { height: `${thickness}rem` } : {}}
+    >
+      <ul>{nav_items}</ul>
     </nav>
   );
 }
+
+Navbar.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      ref: PropTypes.string.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+
+  last_nth: PropTypes.number,
+  thickness: PropTypes.number,
+};
+
+export default Navbar;
